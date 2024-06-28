d0a455555f9f90125c6fbbf84b404db09af56e97
luiztoledosilva <luiztoledosilva30@gmail.com>
2024-06-27 22:30:23 -0300

    criando rotas em django

--- projeto/urls.py
+++ projeto/urls.py
@@ -18,11 +18,23 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
-def my_view(request):
-    return HttpResponse('Uma linda string')
+def home(request):
+    return HttpResponse('HOME')
+
+def sobre(request):
+    return HttpResponse('sobre') 
+
+def contato(request):
+    return HttpResponse('contato')
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     ##path('sobre/', my_view),
-    path('', my_view),
+    path('', home), #HOME
+    path('sobre/', sobre), #sobre
+    path('contato/', contato), #contato
+    
+    
 ]
