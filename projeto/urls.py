531acec06303018250f092ce391cc54e06cb0df0
luiztoledosilva <luiztoledosilva30@gmail.com>
2024-07-01 18:38:20 -0300

    recipes 3

--- projeto/urls.py
+++ projeto/urls.py
@@ -29,7 +29,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('recipes.urls')),
-     path('recipes/',include('recipes.urls')),
-   
+    
+
     
 ]
